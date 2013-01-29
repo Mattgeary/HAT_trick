3f63e04ceaf445bf5fe9d727849a513ea84f6cfb
Matt Geary <mattg533@gmail.com>
2013-01-29 15:02:15 +0000

    Function to simulate movement of tsetse flies between grid cells during modelling - requires habitat and stage to be included plus stochasticity

---
+++ movement.R
@@ -0,0 +1,54 @@
+HAT_move <- function(popn, move, move.prob, hab.grid){
+	for(i in 1:nrow(hab.grid)){
+		for(j in 1:ncol(hab.grid)){
+			if (i-1) <= 0 or (j-1) <=0 or (i+1) >= nrow(hab.grid) or (j+1) >= ncol(hab.grid){
+				if (i-1) <= 0{
+					move[[i]][[j]]	<- move + (popn[[i+1]][[j]]*move.prob)
+	 				move[[i]][[j-1]] <- move.a <-  move[[i]][[j-1]] + (popn[[i]][[j]] * move.prob)
+					move[[i+1]][[j]] <- move.c <- move[[i+1]][[j]] + (popn[[i]][[j]] * move.prob)
+					move[[i]][[j+1]] <- move.d <- move[[i]][[j+1]] + (popn[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.a, move.c, move.d))
+				}
+				if (i+1 <= nrow(hab.grid){
+					move[[i]][[j]]	<- move + (popn[[i-1]][[j]]*move.prob)
+					move[[i-1]][[j]] <- move.b <- move[[i-1]][[j]] + (popn[[i]][[j]] * move.prob)
+					move[[i]][[j-1]] <- move.a <- move[[i]][[j-1]] + (popn[[i]][[j]] * move.prob)
+					move[[i]][[j+1]] <- move.d <- move[[i]][[j+1]] + (popn[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.b, move.a, move.d))
+				}
+				if (j-1 <= 0){
+					move[[i]][[j]]	<- move + (popn[[i]][[j+1]]*move.prob)
+					move[[i-1]][[j]] <- move.a <- move[[i-1]][[j]] + (popn[[i]][[j]] * move.prob)
+					move[[i]][[j+1]] <- move.d <- move[[i]][[j+1]] + (popn[[i]][[j]] * move.prob)
+					move[[i+1]][[j]] <- move.c <- move[[i+1]][[j]] + (popn[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.a, move.d, move.c))
+				}
+				if (j+1 >= ncol(hab.grid){
+					move[[i]][[j]]	<- move + (popn[[i]][[j-1]]*move.prob)
+					move[[i-1]][[j]] <- move.b <- move[[i-1]][[j]] + (popn[[i]][[j]] * move.prob)
+					move[[i]][[j-1]] <- move.a <- move[[i]][[j-1]] + (popn[[i]][[j]] * move.prob)
+					move[[i+1]][[j]] <- move.c <- move[[i+1]][[j]] + (popn[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.b, move.a, move.c))
+				}
+				
+			}
+			else{
+				move[[i]][[j]]	<- move + (popn[[i+1]][[j]]*move.prob)
+	 			move[[i]][[j-1]] <- move.a <-  move[[i]][[j-1]] + (popn[[i]][[j]] * move.prob)
+	 			move[[i-1]][[j]] <- move.b <-  move[[i-1]][[j]] + (popn[[i]][[j]] * move.prob)
+				move[[i+1]][[j]] <- move.c <- move[[i+1]][[j]] + (popn[[i]][[j]] * move.prob)
+				move[[i]][[j+1]] <- move.d <- move[[i]][[j+1]] + (popn[[i]][[j]] * move.prob)
+				popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.a, move.b, move.c, move.d))
+			}
+		}
+	}
+	move.grid <- matrix(NA, nrow=nrow(hab.grid), ncol=ncol(hab.grid))
+	for(i in 1:nrow(hab.grid)){
+		for(j in ncol(hab.grid)){
+			move.grid[i,j] <- sum(move[[i]][[j]])
+		}
+	}
+	return(new.pop = popn, movements = move, move.grid = move.grid)
+}
+				
+			
