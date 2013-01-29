9eac5da9b47ab546e9d60a30312ef5a810c65317
Matt Geary <mattg533@gmail.com>
2013-01-29 15:33:20 +0000

    Or syntax in if function fixed

--- movement.R
+++ movement.R
@@ -1,29 +1,29 @@
 HAT_move <- function(popn, move, move.prob, hab.grid){
 	for(i in 1:nrow(hab.grid)){
 		for(j in 1:ncol(hab.grid)){
-			if (i-1) <= 0 or (j-1) <=0 or (i+1) >= nrow(hab.grid) or (j+1) >= ncol(hab.grid){
-				if (i-1) <= 0{
+			if((i-1) <= 0 | (j-1) <=0 | (i+1) >= nrow(hab.grid) | (j+1) >= ncol(hab.grid)){
+				if(i-1 <= 0){
 					move[[i]][[j]]	<- move + (popn[[i+1]][[j]]*move.prob)
 	 				move[[i]][[j-1]] <- move.a <-  move[[i]][[j-1]] + (popn[[i]][[j]] * move.prob)
 					move[[i+1]][[j]] <- move.c <- move[[i+1]][[j]] + (popn[[i]][[j]] * move.prob)
 					move[[i]][[j+1]] <- move.d <- move[[i]][[j+1]] + (popn[[i]][[j]] * move.prob)
 					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.a, move.c, move.d))
 				}
-				if (i+1 <= nrow(hab.grid){
+				if(i+1 <= nrow(hab.grid)){
 					move[[i]][[j]]	<- move + (popn[[i-1]][[j]]*move.prob)
 					move[[i-1]][[j]] <- move.b <- move[[i-1]][[j]] + (popn[[i]][[j]] * move.prob)
 					move[[i]][[j-1]] <- move.a <- move[[i]][[j-1]] + (popn[[i]][[j]] * move.prob)
 					move[[i]][[j+1]] <- move.d <- move[[i]][[j+1]] + (popn[[i]][[j]] * move.prob)
 					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.b, move.a, move.d))
 				}
-				if (j-1 <= 0){
+				if(j-1 <= 0){
 					move[[i]][[j]]	<- move + (popn[[i]][[j+1]]*move.prob)
 					move[[i-1]][[j]] <- move.a <- move[[i-1]][[j]] + (popn[[i]][[j]] * move.prob)
 					move[[i]][[j+1]] <- move.d <- move[[i]][[j+1]] + (popn[[i]][[j]] * move.prob)
 					move[[i+1]][[j]] <- move.c <- move[[i+1]][[j]] + (popn[[i]][[j]] * move.prob)
 					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.a, move.d, move.c))
 				}
-				if (j+1 >= ncol(hab.grid){
+				if(j+1 >= ncol(hab.grid)){
 					move[[i]][[j]]	<- move + (popn[[i]][[j-1]]*move.prob)
 					move[[i-1]][[j]] <- move.b <- move[[i-1]][[j]] + (popn[[i]][[j]] * move.prob)
 					move[[i]][[j-1]] <- move.a <- move[[i]][[j-1]] + (popn[[i]][[j]] * move.prob)
