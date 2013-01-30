919712c4f62910ad4c91ce191a81be9e929ccce3
Matt Geary <mattg533@gmail.com>
2013-01-30 15:23:09 +0000

    Movement function added to - column and row 10 still do not function correctly

--- movement.R
+++ movement.R
@@ -1,78 +1,86 @@
 HAT_move <- function(popn, move, move.prob, hab.grid){
+	popn.orig <- popn
 	for(i in 1:nrow(hab.grid)){
 		for(j in 1:ncol(hab.grid)){
-			if((i-1) <= 0 | (j-1) <=0 | (i+1) >= nrow(hab.grid) | (j+1) >= ncol(hab.grid)){
+			if((i-1) <= 0 | (j-1) <=0 | i >= nrow(hab.grid) | j >= ncol(hab.grid)){
+				print("yes")
 				if(i-1 <= 0 & j-1 <= 0){
 					move[[i]][[j]]	<- move[[i]][[j]] + ((popn[[(i+1)]][[j]]* move.prob) + (popn[[(i)]][[j+1]]* move.prob))
-					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn[[i]][[j]] * move.prob)
-					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.c, move.d))
+					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn.orig[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - (move.c + move.d)
+					print("i<=0 & j<=0")
 				}
-				if(i-1 <= 0 & j+1 >= ncol(hab.grid)){
+				if(i-1 <= 0 & j >= ncol(hab.grid)){
 					move[[i]][[j]]	<- move[[i]][[j]] + ((popn[[(i+1)]][[j]]* move.prob) + (popn[[(i)]][[j-1]]* move.prob))
-					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					move[[i]][[(j-1)]] <- move.d <- move[[i]][[(j-1)]] + (popn[[i]][[j]] * move.prob)
-					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.c, move.d))
+					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[i]][[(j-1)]] <- move.d <- move[[i]][[(j-1)]] + (popn.orig[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - (move.c + move.d)
+					print("i<=0 & j>=10")
 				}
-				if(i+1 >= nrow(hab.grid) & j-1 <= 0){
+				if(i >= nrow(hab.grid) & (j-1) <= 0){
 					move[[i]][[j]]	<- move[[i]][[j]] + ((popn[[(i-1)]][[j]]* move.prob) + (popn[[(i)]][[j+1]]* move.prob))
-					move[[(i-1)]][[j]] <- move.c <- move[[(i-1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn[[i]][[j]] * move.prob)
-					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.c, move.d))
+					move[[(i-1)]][[j]] <- move.c <- move[[(i-1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn.orig[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - (move.c + move.d)
+					print("i>=10 & j<=0")
 				}
-				if(i+1 >= nrow(hab.grid) & j+1 <= 0){
+				if(i >= nrow(hab.grid) & j >= ncol(hab.grid)){
 					move[[i]][[j]]	<- move[[i]][[j]] + ((popn[[(i-1)]][[j]]* move.prob) + (popn[[(i)]][[j-1]]* move.prob))
-					move[[(i-1)]][[j]] <- move.c <- move[[(i-1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					move[[i]][[(j-1)]] <- move.d <- move[[i]][[(j-1)]] + (popn[[i]][[j]] * move.prob)
-					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.c, move.d))
+					move[[(i-1)]][[j]] <- move.c <- move[[(i-1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[i]][[(j-1)]] <- move.d <- move[[i]][[(j-1)]] + (popn.orig[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - (move.c + move.d)
+					print("i>=10 & j>=10")
 				}
-				if(i-1 <= 0){
+				if(i-1 <= 0 & (j-1) > 0 & j < ncol(hab.grid)){
 					move[[i]][[j]]	<- move[[i]][[j]] + (popn[[(i+1)]][[j]]* move.prob)
-	 				move[[i]][[(j-1)]] <- move.a <-  move[[i]][[(j-1)]] + (popn[[i]][[j]] * move.prob)
-					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn[[i]][[j]] * move.prob)
-					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.a, move.c, move.d))
+	 				move[[i]][[(j-1)]] <- move.a <-  move[[i]][[(j-1)]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn.orig[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - (move.a + move.c + move.d)
 				}
-				if(i+1 >= nrow(hab.grid)){
+				if(i+1 >= nrow(hab.grid) & (j-1) > 0 & j+1 < ncol(hab.grid)){
 					move[[i]][[j]]	<- move[[i]][[j]] + (popn[[(i-1)]][[j]] * move.prob)
-					move[[(i-1)]][[j]] <- move.b <- move[[(i-1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					move[[i]][[(j-1)]] <- move.a <- move[[i]][[(j-1)]] + (popn[[i]][[j]] * move.prob)
-					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn[[i]][[j]] * move.prob)
-					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.b, move.a, move.d))
+					move[[(i-1)]][[j]] <- move.b <- move[[(i-1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[i]][[(j-1)]] <- move.a <- move[[i]][[(j-1)]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn.orig[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - (move.b + move.a + move.d)
 				}
-				if(j-1 <= 0){
+				if(j-1 <= 0 & (i-1) > 0 & i < nrow(hab.grid)){
 					move[[i]][[j]]	<- move[[i]][[j]] + (popn[[i]][[(j+1)]] * move.prob)
-					move[[(i-1)]][[j]] <- move.a <- move[[(i-1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn[[i]][[j]] * move.prob)
-					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.a, move.d, move.c))
+					move[[(i-1)]][[j]] <- move.a <- move[[(i-1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[i]][[(j+1)]] <- move.d <- move[[i]][[(j+1)]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - (move.a + move.d + move.c)
 				}
-				if(j+1 >= ncol(hab.grid)){
+				if(j >= ncol(hab.grid) & (i-1) > 0 & i < nrow(hab.grid)){
 					move[[i]][[j]]	<- move[[i]][[j]] + (popn[[i]][[(j-1)]] * move.prob)
-					move[[(i-1)]][[j]] <- move.b <- move[[(i-1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					move[[i]][[(j-1)]] <- move.a <- move[[i]][[(j-1)]] + (popn[[i]][[j]] * move.prob)
-					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn[[i]][[j]] * move.prob)
-					popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.b, move.a, move.c))
+					move[[(i-1)]][[j]] <- move.b <- move[[(i-1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[i]][[(j-1)]] <- move.a <- move[[i]][[(j-1)]] + (popn.orig[[i]][[j]] * move.prob)
+					move[[(i+1)]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+					popn[[i]][[j]] <- popn[[i]][[j]] - (move.b + move.a + move.c)
 				}
 				
+			} else {
+				move[[i]][[j-1]] <- move.a <-  move[[i]][[(j-1)]] + (popn.orig[[i]][[j]] * move.prob)
+	 			move[[i-1]][[j]] <- move.b <-  move[[(i-1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+				move[[i+1]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn.orig[[i]][[j]] * move.prob)
+				move[[i]][[j+1]] <- move.d <- move[[i]][[(j+1)]] + (popn.orig[[i]][[j]] * move.prob)
+				popn[[i]][[j]] <- popn[[i]][[j]] - (move.a + move.b + move.c + move.d)
 			}
-			else{
-				move[[i]][[j-1]] <- move.a <-  move[[i]][[(j-1)]] + (popn[[i]][[j]] * move.prob)
-	 			move[[i-1]][[j]] <- move.b <-  move[[(i-1)]][[j]] + (popn[[i]][[j]] * move.prob)
-				move[[i+1]][[j]] <- move.c <- move[[(i+1)]][[j]] + (popn[[i]][[j]] * move.prob)
-				move[[i]][[j+1]] <- move.d <- move[[i]][[(j+1)]] + (popn[[i]][[j]] * move.prob)
-				popn[[i]][[j]] <- popn[[i]][[j]] - sum(c(move.a, move.b, move.c, move.d))
-			}
+		print(c(i,j))
 		}
+	print(i)
 	}
-	move.grid <- matrix(NA, nrow=nrow(hab.grid), ncol=ncol(hab.grid))
+	move.grid <- matrix(0, nrow=nrow(hab.grid), ncol=ncol(hab.grid))
 	for(i in 1:nrow(hab.grid)){
-		for(j in ncol(hab.grid)){
+		for(j in 1:ncol(hab.grid)){
+			print(c(i,j))
 			move.grid[i,j] <- sum(move[[i]][[j]])
 		}
 	}
-	return(new.pop = popn, movements = move, move.grid = move.grid)
+	return(list(new.pop = popn, movements = move, move.grid = move.grid))
 }
 				
 			
-move.fun <- HAT_move(cell.popn, move, move.prob, hab.grid)
+
