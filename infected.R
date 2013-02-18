19fa34399847050d6aeb173890d1f1342e73855b
Matt Geary <mattg533@gmail.com>
2013-02-18 13:04:12 +0000

    Remove matrix calculation as already present in main model

--- infected.R
+++ infected.R
@@ -2,7 +2,6 @@ infect <- funtion(cell.popn, pop.mat, infect)
 	for(i in 1:length(cell.popn)){
 		for (j in 1:length(cell.popn[[i]])){
 			N <- cell.popn[[i]][[j]]
-			N <- pop.mat %*% N
 			for(p in 1:max(row.pup){
 				N[p,1] <- N[p,1] + N[p,2]
 				N[p,2] <- 0
